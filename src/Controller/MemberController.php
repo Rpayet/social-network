57750edc5c0ecd0fdace3394ddc7148ab3e9507a
Rpayet <robinpayet@gmail.com>
2023-03-24 16:48:47 +0100

    Page Show

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,19 @@ class MemberController extends AbstractController
         return $this->render('member/index.html.twig', [
             'users' => $repository->findAll(),
         ]);
+    }
+
+    #[Route('/member/{id}', name: 'app_member_show')]
+    public function show(Request $request, UserRepository $repository, int $id): Response
+    {
+        $user = $repository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur inexistant');
+        }
+
+        return $this->render('member/show.html.twig', [
+            'user' => $user,
+            ]);
     }
 }
