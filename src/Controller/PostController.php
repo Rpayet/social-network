43319a35bde3611e4abee931aeaf235ebe4851da
Robin Payet <robinpayet@gmail.com>
2023-03-27 16:40:18 +0200

    Controller New Comment

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,20 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
-use App\Repository\PostRepository;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/post/{id}', name: 'app_post')]
-    public function index(Post $post): Response
+    public function index(Post $post, EntityManagerInterface $manager, Request $request, int $id): Response
     {
-    
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $comment->setUser($this->getUser());
+            $comment->setPost($post);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_post', [], Response::HTTP_SEE_OTHER);
+        }
         return $this->render('post/index.html.twig', [
             'post' => $post,
+            'form' => $form,
+            
         ]);
     }
 }
